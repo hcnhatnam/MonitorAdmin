0b5e4ace0ab0d16637a078b6e22b51f1c5f55762
Hoàng Công Nhật Nam <1512061@hcmut.edu.vn>
2019-05-09 07:11:33 +0700

    a

--- src/main/java/com/monitor/admin/controller/ControllerApp.java
+++ src/main/java/com/monitor/admin/controller/ControllerApp.java
@@ -48,18 +48,18 @@ public class ControllerApp {
 
     @RequestMapping(value = "/index2011", method = RequestMethod.GET)
     public String genIndex2011(HttpServletRequest req) {
-        return "page/dashboard2015";
+        return "page/dashboard2011";
     }
 
     
     @RequestMapping(value = "/index2013", method = RequestMethod.GET)
     public String genIndex2013(HttpServletRequest req) {
-        return "page/dashboard2011";
+        return "page/dashboard2013";
     }
 
     @RequestMapping(value = "/index2015", method = RequestMethod.GET)
     public String genIndex2015(HttpServletRequest req) {
-        return "page/dashboard2013";
+        return "page/dashboard2015";
     }
 
     
