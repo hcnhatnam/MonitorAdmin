df67c84cadf60fb0157b7f22b1dcf405d3abc1f9
Hoàng Công Nhật Nam <1512061@hcmut.edu.vn>
2019-05-18 08:58:53 +0700

    update total request

--- src/main/java/com/monitor/admin/entities/Statistical.java
+++ src/main/java/com/monitor/admin/entities/Statistical.java
@@ -61,24 +61,28 @@ public class Statistical implements Comparable<Statistical> {
             datesInRange.add(result);
             calendar.add(Calendar.DATE, 1);
         }
+        Date result = calendar.getTime();
+        datesInRange.add(result);
+        calendar.add(Calendar.DATE, 1);
         return datesInRange;
     }
 
     public static boolean containsDate(final List<Date> list, final Date date) {
         return list.stream().filter(o -> o.equals(date)).findFirst().isPresent();
     }
+
     public static Optional<Statistical> containsDateStatis(final List<Statistical> list, final Date date) {
         return list.stream().filter(o -> o.timeReq.equals(date)).findFirst();
     }
-    
+
     public static void main(String[] args) {
         Statistical a = new Statistical("1", "2019-04-01");
         Statistical b = new Statistical("2", "2019-04-04");
-        List<Statistical>statisticals=new ArrayList<>(Arrays.asList(a,b));
+        List<Statistical> statisticals = new ArrayList<>(Arrays.asList(a, b));
         System.out.println("44" + a.timeReq);
         List<Date> dates = Statistical.getDatesBetween(a.gettimeReq(), b.gettimeReq());
 
-        System.err.println("===" + Statistical.containsDateStatis(statisticals,a.timeReq));
+        System.err.println("===" + Statistical.containsDateStatis(statisticals, a.timeReq));
 
     }
 }
